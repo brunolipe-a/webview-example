06f0cac6f4647a9654e6bc802a0bfadb327d7d59
Bruno <bfaa1999@gmail.com>
2023-02-24 21:04:46 -0300

    geral

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  BackHandler,
+  Platform,
+  ActivityIndicator,
+  Modal,
+} from "react-native";
+import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
+import { useRef, useEffect, useState } from "react";
+import LoadingModal from "./src/components/LoadingModal";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const webViewRef = useRef(null);
+
+  function onAndroidBackPress() {
+    if (!webViewRef.current) {
+      return false;
+    }
+
+    webViewRef.current.goBack();
+
+    return true; // prevent default behavior (exit app)
+  }
+
+  useEffect(() => {
+    if (Platform.OS !== "android") {
+      return;
+    }
+
+    BackHandler.addEventListener("hardwareBackPress", onAndroidBackPress);
+
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", onAndroidBackPress);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <WebView
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        onLoadStart={() => setIsLoading(true)}
+        onLoadEnd={() => setIsLoading(false)}
+        allowsBackForwardNavigationGestures
+        showsHorizontalScrollIndicator={false}
+        source={{ uri: "https://afsphoto.fotop.com.br/fotos" }}
+        ref={webViewRef}
+      >
+        <LoadingModal isOpen={isLoading} />
+      </WebView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
   },
 });
---
+++ src/components/LoadingModal.js
@@ -0,0 +1,40 @@
+import { ActivityIndicator, Modal, View, StyleSheet } from "react-native";
+
+export default function LoadingModal({ isOpen }) {
+  return (
+    <Modal
+      animationType="fade"
+      visible={isOpen}
+      transparent
+      statusBarTranslucent
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <ActivityIndicator size="small" color="#ff7e00" />
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0008",
+  },
+  modalView: {
+    padding: 16,
+    backgroundColor: "white",
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
